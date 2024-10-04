00567c46b586ceb0cfe2caab7846409444f84e19
galavievaidar@mail.ru <galavievaidar@mail.ru>
2024-10-04 16:30:32 +0300

    Предпоследний урок 2ого раздела

---
+++ women/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, re_path
+from . import views
+from women.views import page_not_found
+
+urlpatterns = [
+    path('', views.index, name='home'),
+
+    path('about/', views.about, name='about'),
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
+]
+
+
+hanlder404 = page_not_found
