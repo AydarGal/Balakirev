8834e70b981db3096e4a3938116df617ce6fd7e1
Admin <galavievaidar@mail.ru>
2024-10-08 00:24:54 +0300

    Слаг

--- women/models.py
+++ women/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Women(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)  # unique - уникальное поле, db_index - индексируемое, чтобы быстрее статьи выбирались из бд
     content = models.TextField(blank=True)  # blank=True - не обязательно заполнять
     time_create = models.DateTimeField(auto_now_add=True)  # auto_now_add - заполняется только при создании
     time_update = models.DateTimeField(auto_now=True)  # auto_now - меняется дата при каждом изменении
@@ -16,3 +18,6 @@ class Women(models.Model):
         indexes = [
             models.Index(fields=['-time_create'])
         ]
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_slug': self.slug})
--- women/views.py
+++ women/views.py
@@ -29,10 +29,11 @@ cats_db = [
 
 
 def index(request):
+    posts = Women.objects.filter(is_published=1)
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': data_db,
+        'posts': posts,
         'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=data)
@@ -42,8 +43,8 @@ def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
-def show_post(request, post_id):
-    post = get_object_or_404(Women, pk=post_id)
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
 
     data = {
         'title': post.title,
