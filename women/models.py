5635162c80f0e087f93f6e2413691da230e3095d
galavievaidar@mail.ru <galavievaidar@mail.ru>
2024-10-11 23:00:29 +0300

    4.4 Отображение постов по рубрикам

--- women/models.py
+++ women/models.py
@@ -18,6 +18,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)  # auto_now_add - заполняется только при создании
     time_update = models.DateTimeField(auto_now=True)  # auto_now - меняется дата при каждом изменении
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)  # default=True - дефолтное значение True
+    cat = models.ForeignKey(to='Category', on_delete=models.PROTECT, related_name='posts')
 
     objects = models.Manager()
     published = PublishedManager()
@@ -33,3 +34,14 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
